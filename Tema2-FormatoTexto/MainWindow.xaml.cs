7078c51f1916bea193a43243045fd91aa92990ba
Jose Alfredo Zaragoza Marco <joshalfre10@gmail.com>
2022-09-29 21:43:57 +0200

    Agregar archivos de proyecto.

---
+++ Tema2-FormatoTexto/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tema2_FormatoTexto
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            resultadoTextBox.IsReadOnly = true;
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            resultadoTextBox.Text = inputTextBox.Text;
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if(rojoRadioButton.IsChecked == true)
+            {
+                resultadoTextBox.Foreground = Brushes.Red;
+            }
+            else if(azulRadioButton.IsChecked == true)
+            {
+                resultadoTextBox.Foreground = Brushes.Blue;
+            }
+            else if(verdeRadioButton.IsChecked == true)
+            {
+                resultadoTextBox.Foreground = Brushes.Green;
+            }
+        }
+
+        private void EstiloCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            if(negritaCheckBox.IsChecked == true)
+            {
+                resultadoTextBox.FontWeight = FontWeights.Bold;
+            }
+            if(cursivaCheckBox.IsChecked == true)
+            {
+                resultadoTextBox.FontStyle = FontStyles.Italic;
+            }
+        }
+
+        private void CheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            if (negritaCheckBox.IsChecked == false)
+            {
+                resultadoTextBox.FontWeight = FontWeights.Normal;
+            }
+            if (cursivaCheckBox.IsChecked == false)
+            {
+                resultadoTextBox.FontStyle = FontStyles.Normal;
+            }
+        }
+    }
+}
